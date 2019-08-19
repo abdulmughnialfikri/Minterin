9ff8f3cd5618fd086edca038c5a234023ce4e29b
abdulmughnialfikri <dev.abdulmughni@gmail.com>
2019-08-19 18:28:28 +0700

    rename file

--- app/src/main/java/mighty13/minterin/LoginActivity.java
+++ app/src/main/java/mighty13/minterin/LoginActivity.java
@@ -82,7 +82,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             } else {
                                 Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(LoginActivity.this, ListVideoActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, DummyLogout.class);
                                 startActivity(intent);
 
                             }
